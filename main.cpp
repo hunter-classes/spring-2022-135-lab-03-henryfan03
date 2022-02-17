a018cdf35aa542b521b0c3f6052b38977951adc8
Henry Fan <henry.fan2313@gmail.com>
2022-02-17 02:52:10 -0500

    submitted part D

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 // add your code as appropriate
 #include "reservoir.h"
+#include "reverseorder.h"
 #include <iostream>
 
 int main()
 {
-  std::cout << "LAB TASK A:\n";
+  std::cout << "\nLAB TASK A:\n";
   std::cout << "-------------------------------------\n\n";
   std::cout << "get_east_storage(01/01/2018): " << get_east_storage("01/01/2018") << std::endl;
   std::cout << "get_east_storage(02/22/2018): " << get_east_storage("02/22/2018") << std::endl;
@@ -17,6 +18,15 @@ int main()
   std::cout << "\n\n\n";
   std::cout << "LAB TASK C:\n";
   std::cout << "-------------------------------------\n\n";
-  
+  std::cout << "compare_basinse(01/01/2018): " << compare_basins("01/01/2018") << std::endl;
+  std::cout << "compare_basins(02/22/2018): " << compare_basins("02/22/2018") << std::endl;
+  std::cout << "compare_basins(04/29/2018): " << compare_basins("04/29/2018") << std::endl;
+  std::cout << "\n\n\n";
+  std::cout << "LAB TASK D:\n";
+  std::cout << "-------------------------------------\n\n";
+  std::cout << "reverse_order('05/29/2018','06/02/2018'):\n";
+  reverse_order("05/29/2018","06/02/2018");
+  std::cout << "\n\n\n";
+
   return 0;
 }
